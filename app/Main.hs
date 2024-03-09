45dca0e4836eb52fa29dfb867bb474127ff397bf
iambasantarai <iambasantarai@gmail.com>
2024-03-09 22:42:43 +0545

    Build ui and add logic for aalu-cross i.e. tic-tac-toe

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,14 @@
 module Main where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.Color
 
-window = InWindow "AALU CROSS" (640, 480) (100, 100)
+import Game
+import Logic
+import Rendering
 
+window = InWindow "AALU CROSS" (screenWidth, screenHeight) (100, 100)
 backgroundColor = makeColor 0 0 0 255
-
-initialGame = 42
-
-gameAsPicture _ = Blank
-
-transformGame _ game = game
 
 main :: IO ()
 main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
