eee50fbd3c2d6ffcf637928ef609661ffd5eabc3
iambasantarai <iambasantarai@gmail.com>
2024-03-09 21:42:09 +0545

    Implement basic gloss application

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Graphics.Gloss
+
+window:: Display
+window = InWindow "AALU CROSS" (640, 480) (100, 100)
+
+background :: Color
+background = makeColor 255 255 255 255
+
+drawing :: Picture
+drawing = circle 160
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = display window background drawing
